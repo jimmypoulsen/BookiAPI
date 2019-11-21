9c222ffb4a933f9b928916aad8f810fa66f4a7ab
markdalsgaard <markdalsgaardhansen@gmail.com>
2019-11-21 14:28:14 +0100

    Added funtionality for Venues

---
+++ BookiAPI.DataAccessLayer/VenueRepository.cs
@@ -0,0 +1,42 @@
+﻿using BookiAPI.DataAccessLayer.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookiAPI.DataAccessLayer
+{
+   public class VenueRepository
+    {
+        public IEnumerable<Venue> Get(int id = 0)
+        {
+            const string SELECT_SQL = @"SELECT *
+                                        FROM Venues;";
+
+            using (var conn = Database.Open())
+            {
+                var data = conn.Query<Venue>(SELECT_SQL);
+                if (id != 0)
+                {
+                    return data.Where(v => v.Id == id);
+                }
+                return data;
+            }
+        }
+
+        public bool Add(Venue venue)
+        {
+            const string INSERT_SQL = @"INSERT INTO Venues
+                                        (Address, Zip, City)
+                                        VALUES (@address, @zip, @city);";
+
+            using (var conn = Database.Open())
+            {
+                var rows = conn.Execute(INSERT_SQL, venue);
+                return rows == 1;
+            }
+        }
+    }
+}
---
+++ BookiAPI.RESTfulService/Models/VenueResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookiAPI.RESTfulService.Models
+{
+    public class VenueResponse
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public int Zip { get; set; }
+        
+    }
+}
