5a3909f084039e8edc0f2b4621aee9ae38ceb74b
Jimmy Poulsen <jimmy@progras.dk>
2019-11-22 11:39:36 +0100

    Merge venue stuff

---
+++ BookiAPI.DataAccessLayer/Models/Venue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookiAPI.DataAccessLayer.Models
+{
+    public class Venue
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public int Zip { get; set; }
+        public string City { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"[{Id}] - {Address} {Zip} {City}";
+        }
+
+    }
+}
---
+++ BookiAPI.RESTfulService/Controllers/VenuesController.cs
@@ -0,0 +1,63 @@
+﻿using BookiAPI.DataAccessLayer;
+using BookiAPI.RESTfulService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BookiAPI.RESTfulService.Controllers
+{
+    public class VenuesController : ApiController
+    {
+        private readonly VenueRepository _venueRepository;
+
+        public VenuesController()
+        {
+            _venueRepository = new VenueRepository();
+        }
+
+        // GET /api/venue/
+        public IEnumerable<VenueResponse> Get()
+        {
+            return _venueRepository.Get().Select(venue => new VenueResponse
+            {
+                Id = venue.Id,
+                Address = venue.Address,
+                City = venue.City,
+                Zip = venue.Zip,               
+                
+            });
+        }
+
+        // GET /api/venue/1/
+        public IEnumerable<VenueResponse> Get(int id)
+        {
+            return _venueRepository.Get(id).Select(venue => new VenueResponse
+            {
+                Id = venue.Id,
+                Address = venue.Address,
+                City = venue.City,
+                Zip = venue.Zip,          
+            });
+        }
+
+        // POST /api/venue/
+        // body: JSON
+        public IHttpActionResult Post([FromBody]dynamic data)
+        {
+            BookiAPI.DataAccessLayer.Models.Venue venue = new DataAccessLayer.Models.Venue
+            {
+                Address = data.Venue.Address.Value,
+                City = data.Venue.City.Value,
+                Zip = (int)data.Venue.Zip.Value,
+            };
+
+            if (_venueRepository.Add(venue))
+                return Ok("Venue was created");
+            else
+                return BadRequest("Something went wrong ..");
+        }
+    }
+}
