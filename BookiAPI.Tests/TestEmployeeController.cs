532e2f4cefad92574aa6da37bf8031113cca1015
markdalsgaard <markdalsgaardhansen@gmail.com>
2019-12-10 10:02:07 +0100

    Added tests, now correct

---
+++ BookiAPI.Tests/TestEmployeeController.cs
@@ -0,0 +1,56 @@
+﻿using BookiAPI.RESTfulService.Controllers;
+using BookiAPI.RESTfulService.Models;
+using BookiAPI.DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace BookiAPI.Tests
+{
+    [TestClass]
+    public class TestEmployeesController
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+
+        }
+
+        [TestMethod]
+
+        public void GetEmployeeThatExist()
+        {
+            //arrange
+            EmployeesController _employeesController = new EmployeesController();
+            int expectedEmployeeId = 1;
+
+            //act
+            EmployeeResponse employeeResponse = _employeesController.Get(expectedEmployeeId).First();
+            int acutalEmployeeId = employeeResponse.Id;
+
+            //assert
+            Assert.AreEqual(expectedEmployeeId, acutalEmployeeId);
+
+        }
+
+        [TestMethod]
+        public void GetEmployeeThatDoesNotExcist()
+        {
+            //arrange
+            EmployeesController _employeesController = new EmployeesController();
+            int expectedEmployeeId = 99;
+
+            //act
+            EmployeeResponse employeeResponse = _employeesController.Get(1).First();
+            int acutalEmployeeId = employeeResponse.Id;
+
+            //assert
+            Assert.AreNotEqual(expectedEmployeeId, acutalEmployeeId);
+        }
+
+
+    }
+
+}
---
+++ BookiAPI.Tests/TestReservationsController.cs
@@ -0,0 +1,49 @@
+﻿using BookiAPI.RESTfulService.Controllers;
+using BookiAPI.RESTfulService.Models;
+using BookiAPI.DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace BookiAPI.Tests
+{
+    [TestClass]
+    public class TestReservationsController
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetReservationThatExcist()
+        {
+            //Arange
+            ReservationsController _reservationsController = new ReservationsController();
+            int expectedReservationsId = 2;
+
+            //Act
+            ReservationResponse reservationResponse = _reservationsController.Get(expectedReservationsId).First();
+            int actualReservationsId = reservationResponse.Id;
+
+            //Assert
+            Assert.AreEqual(expectedReservationsId, actualReservationsId);
+
+        }
+
+        [TestMethod]
+        public void GetReservationThatDosentExcist()
+        {
+            //Arange
+            ReservationsController _reservationsController = new ReservationsController();
+            int 
+
+            //Act
+
+            //Assert
+        }
+    }
+}
